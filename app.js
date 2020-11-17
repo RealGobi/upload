591c536180fe1802eab4a56f19c330fc8bf7542d
Jimmy Svensson <jimmy888swe@gmail.com>
2020-11-17 15:10:52 +0100

    feat: route: get and render one image

--- app.js
+++ app.js
@@ -104,6 +104,29 @@ app.get('/files/:filename', (req,res) => {
     })
 });
 
+// GET /image/:filename
+
+app.get('/image/:filename', (req,res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+      // check if file
+      if(!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file found'
+        })
+      }
+      // check if image
+
+    if(file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+      }
+    })
+});
+
 const PORT = 4000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
